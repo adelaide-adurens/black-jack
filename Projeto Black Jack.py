e9cfbda7e34924a69848e1283bc4f9786a793dd1
adelaide-adurens <adelaideadurens@gmail.com>
2021-07-29 14:44:50 -0300

    Correções

--- Projeto Black Jack.py
+++ Projeto Black Jack.py
@@ -1,5 +1,8 @@
+#JOGO BLACKJACK OU 21 - NÃO HÁ MESA E BARALHO É ÚNICO - OBJETIVO DO JOGO É COMPLETAR 21 PONTOS OU O MÁXIMO, SEM PASSAR DE 21, ENTRE O NÚMERO DE PARTICIPANTES
+
 import random
 
+#Função que cria o baralho com as cartas e respectivos valores para o jogo
 def criarBaralho():
     baralho = []
     carta = []
@@ -21,23 +24,26 @@ def criarBaralho():
             baralho.append(carta)
     return baralho
 
+#Função que sorteia uma carta do baralho
 def sorteio(listaBaralho):
     return random.choice(listaBaralho)
 
+#Função que valida o Sim ou Não para continuar a tirar cartas durante o jogo
 def entradaValida(entrada):
     valido = True
     entradasValidas = ['S', 'N']
     if entrada not in entradasValidas:
         valido = False
-        print("Jogada Inválida. Digite 'S' para Sim e 'N' para Não.")      
+        print("Jogada Inválida. Digite 'S' para Sim e 'N' para Não.\n")      
     return valido
 
+#Função que verifica se o jogador ainda está ativo e se ainda quer tirar mais cartas e inativa o jogador caso ele some 21 pontos ou passe de 21.
 def jogada(jogador, baralho):
     cartaSorteada = sorteio(baralho)
     jogadaInvalida = False
     while jogadaInvalida == False:
         if jogador[1] == 'A':
-            continua = input(f"{jogador[0]}, você deseja tirar outra carta? S/N")
+            continua = input(f"{jogador[0]}, você deseja tirar uma carta? S/N\n")
             jogadaInvalida = entradaValida(continua)
         if jogadaInvalida == True:
             if continua == 'N':
@@ -46,12 +52,13 @@ def jogada(jogador, baralho):
                 jogador[2] = jogador[2] + cartaSorteada[1]
                 if jogador[2] == 21:
                     jogador[1] = 'I'
-                    print(f"{jogador[0]}, sua carta foi {cartaSorteada[0]} e sua pontuação está em {jogador[2]} e você ganhou o jogo.")
+                    print(f"{jogador[0]}, sua carta foi {cartaSorteada[0]} e sua pontuação está em {jogador[2]} e você ganhou o jogo.\n")
                 elif jogador[2] > 21:
                     jogador[1] = 'I'
-                    print(f"{jogador[0]}, sua carta foi {cartaSorteada[0]} e sua pontuação está em {jogador[2]} e você perdeu o jogo.")
+                    print(f"{jogador[0]}, sua carta foi {cartaSorteada[0]} e sua pontuação está em {jogador[2]} e você perdeu o jogo.\n")
     return jogador, cartaSorteada  
 
+#Função que verifica qual jogador tirou 21 pontos ou o que alcançou mais pontos durante as jogadas
 def verifica(pontuacao):
     maiorNumero = 0
     maiorPosicao = ''
@@ -60,18 +67,21 @@ def verifica(pontuacao):
             if element[2] > maiorNumero:
                 maiorNumero = element[2]
                 maiorPosicao = element[0]
-                vencedor = f"{maiorPosicao}, você alcançou a maior pontuação." 
+                vencedor = f"{maiorPosicao}, você alcançou a maior pontuação.\n" 
+            elif element[2] == maiorNumero:
+                vencedor += f"{element[0]}, você também alcançou a maior pontuação.\n" 
     return vencedor
 
-def funcaoPrincipal ():
+#Função prinicpal que pede o número e nome de jogadores e chama as demais funções para que o jogo aconteceça
+def main():
     baralho = criarBaralho()
-    numeroJogadores = int(input("Quantos são os jogadores?"))
+    numeroJogadores = int(input("Quantos são os jogadores?\n"))
     n = 1
     nomeJogadores = []
     jogador = []
 
     while n <= numeroJogadores:
-        nomeJogador = input(f"Qual o nome do {n}º jogador?")
+        nomeJogador = input(f"Qual o nome do {n}º jogador?\n")
         jogador = [nomeJogador, 'A', 0]
         nomeJogadores.append(jogador)
         n +=1
@@ -84,10 +94,11 @@ def funcaoPrincipal ():
                 cont+=1
                 break
             else:
-                print(f"{jogador[0]}, sua carta foi {cartaSorteada[0]} e sua pontuação está em {nomeJogadores[cont][2]}")
+                print(f"{jogador[0]}, sua carta foi {cartaSorteada[0]} e sua pontuação está em {nomeJogadores[cont][2]}\n")
                 nomeJogadores[cont][1] = jogador[1]
                 nomeJogadores[cont][2] = jogador[2]            
 
     print(verifica(nomeJogadores))
 
-funcaoPrincipal()
+if __name__ =="__main__":
+    main()
